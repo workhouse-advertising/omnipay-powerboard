adfc133bc514bcd3bff5b4e19b9a7c4a74fb5f64
Simon Geoghegan <simon@workhouseadvertising.com.au>
2025-08-18 13:40:01 +0800

    Updated documented return types for some Checkout requests

--- src/CheckoutGateway.php
+++ src/CheckoutGateway.php
@@ -18,43 +18,51 @@ class CheckoutGateway extends AbstractGateway
      * Create a checkout intent.
      *
      * @param array $options
-     * @return \Omnipay\Common\Message\AbstractRequest
+     * @return \Omnipay\Powerboard\Message\Checkout\CreateIntentRequest
      */
     public function createIntent(array $options = [])
     {
-        return $this->createRequest(\Omnipay\Powerboard\Message\Checkout\CreateIntentRequest::class, $options);
+        $request = $this->createRequest(\Omnipay\Powerboard\Message\Checkout\CreateIntentRequest::class, $options);
+        /** @var \Omnipay\Powerboard\Message\Checkout\CreateIntentRequest $request */
+        return $request;
     }
 
     /**
      * Get a checkout intent.
      *
      * @param array $options
-     * @return \Omnipay\Common\Message\AbstractRequest
+     * @return \Omnipay\Powerboard\Message\Checkout\GetIntentRequest
      */
     public function getIntent(array $options = [])
     {
-        return $this->createRequest(\Omnipay\Powerboard\Message\Checkout\GetIntentRequest::class, $options);
+        $request = $this->createRequest(\Omnipay\Powerboard\Message\Checkout\GetIntentRequest::class, $options);
+        /** @var \Omnipay\Powerboard\Message\Checkout\GetIntentRequest $request */
+        return $request;
     }
 
     /**
      * Authorize and immediately capture an amount on the customers card
      *
      * @param array $options
-     * @return \Omnipay\Common\Message\AbstractRequest
+     * @return \Omnipay\Powerboard\Message\Checkout\PurchaseRequest
      */
     public function purchase(array $options = [])
     {
-        return $this->createRequest(\Omnipay\Powerboard\Message\Checkout\PurchaseRequest::class, $options);
+        $request = $this->createRequest(\Omnipay\Powerboard\Message\Checkout\PurchaseRequest::class, $options);
+        /** @var \Omnipay\Powerboard\Message\Checkout\PurchaseRequest $request */
+        return $request;
     }
 
     /**
      * Handle return from off-site gateways after purchase
      *
      * @param array $options
-     * @return \Omnipay\Common\Message\AbstractRequest
+     * @return \Omnipay\Powerboard\Message\Checkout\CompletePurchaseRequest
      */
     public function completePurchase(array $options = [])
     {
-        return $this->createRequest(\Omnipay\Powerboard\Message\Checkout\CompletePurchaseRequest::class, $options);
+        $request = $this->createRequest(\Omnipay\Powerboard\Message\Checkout\CompletePurchaseRequest::class, $options);
+        /** @var \Omnipay\Powerboard\Message\Checkout\CompletePurchaseRequest $request */
+        return $request;
     }
 }
