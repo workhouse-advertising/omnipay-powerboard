497940d0283b64343493bd2ec9bdbb4e237f8749
Simon Geoghegan <simon@workhouseadvertising.com.au>
2025-08-19 17:16:12 +0800

    More work-in-progress on experimenting with authentication handling as a redirect

--- src/Message/Card/CompletePurchaseRequest.php
+++ src/Message/Card/CompletePurchaseRequest.php
@@ -9,8 +9,57 @@ class CompletePurchaseRequest extends PurchaseRequest
     /**
      * @inheritDoc
      */
+    public function getData()
+    {
+        $data = parent::getData();
+
+        if ($this->get3dsRequired()) {
+            $this->validate('charge3dsId');
+            $data['_3ds']['id'] = $this->getCharge3dsId();
+        }
+
+        return $data;
+    }
+
+    /**
+     * @inheritDoc
+     */
     public function getResponseClass(): string
     {
         return \Omnipay\Powerboard\Message\Card\CompletePurchaseResponse::class;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function get3dsRequired()
+    {
+        return $this->getParameter('3dsRequired');
+    }
+
+    /**
+     * @param mixed $value
+     * @return $this
+     */
+    public function set3dsRequired($value)
+    {
+        return $this->setParameter('3dsRequired', $value);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCharge3dsId()
+    {
+        return $this->getParameter('charge3dsId');
+    }
+
+    /**
+     * @param mixed $value
+     * @return $this
+     */
+    public function setCharge3dsId($value)
+    {
+        return $this->setParameter('charge3dsId', $value);
     }
 }
