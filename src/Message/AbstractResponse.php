290cf7a5ebd94ed474e2f621e4b50b5b2872759a
Simon Geoghegan <simon@workhouseadvertising.com.au>
2025-08-26 12:18:46 +0800

    Implemented more tests for the Card gateway

--- src/Message/AbstractResponse.php
+++ src/Message/AbstractResponse.php
@@ -15,6 +15,7 @@ abstract class AbstractResponse extends BaseAbstractResponse implements Redirect
     protected $errorCodeMessages = [
         // TODO: Map all expected error codes to a user friendly string that does not
         //       expose the actual error reason.
+        'unfulfilled_condition' => 'There was an error processing the payment due to an unfulfilled condition',
         'unspecified_error' => 'An unspecified error occurred',
         'validation_error' => 'There was an error validating your request',
     ];
